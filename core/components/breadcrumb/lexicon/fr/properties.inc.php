85acb3b454766846c410b558d55ed71d2ce916ac
Benjamin Vauchel <contact@omycode.fr>
2012-08-28 14:03:07 +0200

    French translation added

---
+++ core/components/breadcrumb/lexicon/fr/properties.inc.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * BreadCrumb
+ *
+ *
+ *
+ * BreadCrumb is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * BreadCrumb is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * BreadCrumb; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package breadcrumb
+ */
+/**
+ * Properties (property descriptions) Lexicon Topic
+ *
+ * @package breadcrumb
+ * @subpackage lexicon
+ */
+
+/* BreadCrumb Property Description Example strings */
+
+$_lang['breadcrumb_snippet_from_desc']                 = 'ID de la première ressource du fil d\'Ariane. Par défaut, celui de la page d\'accueil.';
+$_lang['breadcrumb_snippet_to_desc']                   = 'ID de la dernière ressource du fil d\'Ariane. Par défaut, celui de la ressource courante.';
+$_lang['breadcrumb_snippet_maxcrumbs_desc']            = 'Nombre maximum de ressources affichées dans le fil d\'Ariane.';
+$_lang['breadcrumb_snippet_showhidden_desc']           = 'Afficher les ressources cachées.';
+$_lang['breadcrumb_snippet_showcontainer_desc']        = 'Afficher les conteneurs.';
+$_lang['breadcrumb_snippet_showunpub_desc']            = 'Afficher les ressources non publiées.';
+$_lang['breadcrumb_snippet_showcurrentcrumb_desc']     = 'Afficher la ressource courante.';
+$_lang['breadcrumb_snippet_showbreadcrumbatHome_desc'] = 'Afficher le fil d\'Ariane sur la page d\'accueil';
+$_lang['breadcrumb_snippet_showhomecrumb_desc']        = 'Afficher la page d\'accueil dans le fil d\'Ariane';
+$_lang['breadcrumb_snippet_useweblinkurl_desc']        = 'Dans le cas des weblink, utiliser les url spécifiées plutôt que les urls vers ces ressources.';
+$_lang['breadcrumb_snippet_direction_desc']            = 'Direction du fil d\'Ariane : gauche à droite (ltr) ou droite à gauche (rtl) pour l\'arabe par exemple.';
+$_lang['breadcrumb_snippet_scheme_desc']               = 'Schema de génération des URLs.';
+$_lang['breadcrumb_snippet_containertpl_desc']         = 'Template global. Peut prendre pour valeur le chemin vers un fichier (@FILE:), du code (@CODE:) ou le nom d\'un chunk.';
+$_lang['breadcrumb_snippet_currentcrumbtpl_desc']      = 'Template de la ressource courante. Peut prendre pour valeur le chemin vers un fichier (@FILE:), du code (@CODE:) ou le nom d\'un chunk.';
+$_lang['breadcrumb_snippet_linkcrumbtpl_desc']         = 'Template des autres ressources du fil d\'Ariane. Peut prendre pour valeur le chemin vers un fichier (@FILE:), du code (@CODE:) ou le nom d\'un chunk.';
+$_lang['breadcrumb_snippet_maxcrumbtpl_desc']          = 'Template du délimiteur quand la limite max a été atteinte. Peut prendre pour valeur le chemin vers un fichier (@FILE:), du code (@CODE:) ou le nom d\'un chunk.';
